28b41ad313c3130758b6f922413e6db43c814f45
vincent <0426vincent@gmail.com>
2022-12-05 02:00:14 +0800

    fix: fix hadling new results

--- assets/migrations/000010_user_to_results.up.sql
+++ assets/migrations/000010_user_to_results.up.sql
@@ -1,8 +1,9 @@
-CREATE TABLE "user_to_results" (
+CREATE TABLE user_to_results (
     user_id uuid NOT NULL,
     school_name VARCHAR(255) NOT NULL,
     major_name VARCHAR(255) NOT NULL,
     announce_date DATE NOT NULL,
+    status VARCHAR(255) NOT NULL,
     others VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, school_name, major_name, announce_date)
 );
