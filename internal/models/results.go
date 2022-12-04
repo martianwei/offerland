f9bf62dbc0e388c0878572eb3705b8df4e3aeebc
vincent <0426vincent@gmail.com>
2022-12-05 02:00:14 +0800

    fix: fix hadling new results

--- internal/models/results.go
+++ internal/models/results.go
@@ -10,44 +10,38 @@ type ResultModel struct {
 	DB *sql.DB
 }
 
-type AdmittedSchool struct {
-	SchoolName   string `json:"school_name"`
-	MajorName    string `json:"major_name"`
-	AnnounceDate string `json:"announce_date"`
-	Others       string `json:"others"`
-}
-
-type RejectedSchool struct {
-	SchoolName   string `json:"school_name"`
-	MajorName    string `json:"major_name"`
-	AnnounceDate string `json:"announce_date"`
-	Others       string `json:"others"`
-}
-
 type Result struct {
-	UserID     uuid.UUID `json:"user_id"`
-	SchoolName string    `json:"school_name"`
-	MajorName  string    `json:"major_name"`
-	Date       int       `json:"date"`
-	Status     string    `json:"status"`
-	Others     string    `json:"others"`
+	UserID       uuid.UUID `json:"user_id"`
+	SchoolName   string    `json:"school_name"`
+	MajorName    string    `json:"major_name"`
+	AnnounceDate string    `json:"announce_date"`
+	Status       string    `json:"status"`
+	Others       string    `json:"others"`
 }
 
-func (m *ResultModel) Insert(userID uuid.UUID, schoolName string, majorName string, announceDate string, others string) error {
+func (m *ResultModel) Delete(userID uuid.UUID) error {
+	query := `
+		DELETE FROM user_to_results
+		WHERE user_id = $1
+	`
+	_, err := m.DB.Exec(query, userID)
+	return err
+}
+
+func (m *ResultModel) Insert(userID uuid.UUID, schoolName string, majorName string, announceDate string, status string, others string) error {
 	// Insert only unique results
 	query := `
-		INSERT INTO results (user_id, school_name, major_name, announce_date, others)
-		VALUES ($1, $2, $3, $4, $5)
-		ON CONFLICT (user_id, school_name, major_name, announce_date, others) DO NOTHING
+		INSERT INTO user_to_results (user_id, school_name, major_name, announce_date, status, others)
+		VALUES ($1, $2, $3, $4, $5, $6)
 	`
 
-	_, err := m.DB.Exec(query, userID, schoolName, majorName, announceDate, others)
+	_, err := m.DB.Exec(query, userID, schoolName, majorName, announceDate, status, others)
 	return err
 }
 
 func (m *ResultModel) Get(userID uuid.UUID) ([]Result, error) {
 	query := `
-		SELECT user_id, school_name, major_name, announce_date, others
+		SELECT user_id, school_name, major_name, announce_date, status, others
 		FROM user_to_results
 		WHERE user_id = $1
 	`
@@ -61,7 +55,7 @@ func (m *ResultModel) Get(userID uuid.UUID) ([]Result, error) {
 	results := []Result{}
 	for rows.Next() {
 		var r Result
-		err = rows.Scan(&r.UserID, &r.SchoolName, &r.MajorName, &r.Date, &r.Others)
+		err = rows.Scan(&r.UserID, &r.SchoolName, &r.MajorName, &r.AnnounceDate, &r.Others)
 		if err != nil {
 			return nil, err
 		}
